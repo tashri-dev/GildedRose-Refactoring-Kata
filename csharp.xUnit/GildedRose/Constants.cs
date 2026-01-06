34acbfada4a4bc62f31c858c9f0dfd34fe184b2f
Taha <taha.ashri.dv@gmail.com>
2026-01-07 00:46:11 +0400

    xc

--- csharp.xUnit/GildedRose/Constants.cs
+++ csharp.xUnit/GildedRose/Constants.cs
@@ -2,6 +2,7 @@ namespace GildedRoseKata;
 public static class Constants
 {
   public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-    public const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+  public const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+  public const string AgedBarie = "Aged Brie";
 
 }
