bc3d59fe404bb2692476d038c71d0a5f5f405bff
Taha <taha.ashri.dv@gmail.com>
2026-01-07 00:48:34 +0400

    add unit test

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -14,4 +14,16 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.Equal("foo", Items[0].Name);
     }
+
+    [Fact]
+    public void Sulfuras_Should_Do_Nothing(){
+
+        //arrange
+        IList<Item> Items = new List<Item> { new Item { Name = Constants.Sulfuras, SellIn = 0, Quality = 0 } };
+        //act
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        //assert
+        Assert.Equal(0, Items[0].Quality);
+    }
 }
