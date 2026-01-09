5303f6bcf428c20a22a0ac09c148800a08547e4b
Taha <taha.ashri.dv@gmail.com>
2026-01-09 23:27:36 +0400

    mm

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -5,4 +5,78 @@ public class Item
     public string Name { get; set; }
     public int SellIn { get; set; }
     public int Quality { get; set; }
+
+
+    public void UpdateQuality()
+    {
+        if (this.Name != "Aged Brie" && this.Name != "Backstage passes to a TAFKAL80ETC concert")
+        {
+            if (this.Quality > 0)
+            {
+                if (this.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    this.Quality = this.Quality - 1;
+                }
+            }
+        }
+        else
+        {
+            if (this.Quality < 50)
+            {
+                this.Quality = this.Quality + 1;
+
+                if (this.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (this.SellIn < 11)
+                    {
+                        if (this.Quality < 50)
+                        {
+                            this.Quality = this.Quality + 1;
+                        }
+                    }
+
+                    if (this.SellIn < 6)
+                    {
+                        if (this.Quality < 50)
+                        {
+                            this.Quality = this.Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (this.Name != "Sulfuras, Hand of Ragnaros")
+        {
+            this.SellIn = this.SellIn - 1;
+        }
+
+        if (this.SellIn < 0)
+        {
+            if (this.Name != "Aged Brie")
+            {
+                if (this.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (this.Quality > 0)
+                    {
+                        if (this.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            this.Quality = this.Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    this.Quality = this.Quality - this.Quality;
+                }
+            }
+            else
+            {
+                if (this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+                }
+            }
+        }
+    }
 }
