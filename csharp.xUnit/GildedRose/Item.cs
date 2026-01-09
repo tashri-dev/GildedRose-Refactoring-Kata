12c867509335e0138e8cbd7bc71713b2023f5b89
Taha <taha.ashri.dv@gmail.com>
2026-01-10 01:01:21 +0400

    simplify calc

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -82,7 +82,7 @@ public class Item
             {
                 if (Quality < 50)
                 {
-                    Quality = Quality + 1;
+                    Quality ++;
                 }
             }
         }
