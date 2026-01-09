71601e9f7520dd666d851c7d2165e3fda02cef58
Taha <taha.ashri.dv@gmail.com>
2026-01-10 01:00:27 +0400

    simplify calc

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -26,7 +26,7 @@ public class Item
         {
             if (Quality < 50)
             {
-                Quality = Quality + 1;
+                Quality++;
 
                 if (Name == Constants.Backstage)
                 {
